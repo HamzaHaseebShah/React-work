a74a29b7d3759b135c0cc656daf4b08b412bbc3b
Hamza Haseeb <81779041+HamzaHaseebShah@users.noreply.github.com>
2022-02-04 16:49:57 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import EditorialPolicy from './EditorialPolicy';
+
+
+
+function App() {
+  return (
+      <>
+          <EditorialPolicy/>
+          <p>asdsaadaassad</p>
+      </>
+  );
+}
+
+export default App;
---
+++ src/EditorialPolicy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function EditorialPolicy(){
+    return
+     <>
+        <h1>Hamza Haseeb Shah</h1>
+
+    </>
+  }
+  
+  export default EditorialPolicy;
