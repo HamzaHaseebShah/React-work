7e654d9e8197b241d4cfe05842e7a39230b36683
Hamza Haseeb <hamza.haseeb@healthwire.pk>
2022-02-04 15:18:12 +0500

    Updates

---
+++ precache-manifest.ed1aee3c0b193a029e4508e29f13cb39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564fd35411a52b19f567d831961d6840",
+    "url": "/React-work/index.html"
+  },
+  {
+    "revision": "3fe20a57b2d8ec68ae8f",
+    "url": "/React-work/static/css/2.ac0cf94b.chunk.css"
+  },
+  {
+    "revision": "42eb9c3957a674a4b35a",
+    "url": "/React-work/static/css/main.ececb02c.chunk.css"
+  },
+  {
+    "revision": "3fe20a57b2d8ec68ae8f",
+    "url": "/React-work/static/js/2.b2ecfea6.chunk.js"
+  },
+  {
+    "revision": "afe63a26f2d2f3628825",
+    "url": "/React-work/static/js/3.0d00e878.chunk.js"
+  },
+  {
+    "revision": "42eb9c3957a674a4b35a",
+    "url": "/React-work/static/js/main.cb094ad8.chunk.js"
+  },
+  {
+    "revision": "cce8e0849ebeee285f58",
+    "url": "/React-work/static/js/runtime~main.53eb3c2a.js"
+  },
+  {
+    "revision": "6a9c39fdf53c2c66fa13f0cb6969e2e5",
+    "url": "/React-work/static/media/DocotrLogin.6a9c39fd.bin"
+  },
+  {
+    "revision": "58065a832afed248dbcf7094b27e1c46",
+    "url": "/React-work/static/media/Patient-Login.58065a83.png"
+  },
+  {
+    "revision": "81ef7ab82f796ece9d34b1e30c9126ee",
+    "url": "/React-work/static/media/backgroundimage.81ef7ab8.png"
+  }
+]);
